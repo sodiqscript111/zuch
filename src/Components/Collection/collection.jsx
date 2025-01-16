71676d85a09fa14bf3fd467a4778aa2dddc2278e
simidusodiqt <simidusodiqt@gmail.com>
2025-01-16 10:16:51 +0000

    UI changes

--- src/Components/Collection/collection.jsx
+++ src/Components/Collection/collection.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Poise from '../Poise/poise';
 import Elegant from '../Elegant/elegant';
 import Dapper from '../Dapper/dapper';
+import Slider from '../Slider/slider';
+import './collection.css';
+
 const Collection = () => {
     return (
         <div>
@@ -10,7 +13,9 @@ const Collection = () => {
                     <Poise/>
                 </div>
                 <div className="subcollection">
+                
                     <div className="othercollection">
+                        <Slider/>
                         <Elegant/>
                         <Dapper/>
                     </div>
---
+++ src/Components/Slider/slider.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// Import required modules
+import { Navigation, Pagination } from "swiper/modules";
+
+import "./slider.css";
+
+const Slider= ()=> {
+  const collections = [
+    {
+      image: "https://i.ibb.co/NsHKvHh/Whats-App-Image-2025-01-13-at-12-35-01-PM-1.jpg",
+      name: "Collection One",
+    },
+    {
+      image: "https://i.ibb.co/cbxgbmF/Whats-App-Image-2025-01-13-at-12-35-01-PM-2.jpg",
+      name: "Collection Two",
+    },
+    {
+      image: "https://i.ibb.co/QJbzDtB/Whats-App-Image-2025-01-13-at-12-35-01-PM-3.jpg",
+      name: "Collection Three",
+    },
+    {
+      image: "https://i.ibb.co/FkQ1dz4/Whats-App-Image-2025-01-13-at-12-34-59-PM.jpg",
+      name: "Collection Four",
+    },
+    {
+      image: "https://i.ibb.co/FkQ1dz4/Whats-App-Image-2025-01-13-at-12-34-59-PM.jpg",
+      name: "Collection Five",
+    },
+    {
+      image: "https://i.ibb.co/FkQ1dz4/Whats-App-Image-2025-01-13-at-12-34-59-PM.jpg",
+      name: "Collection Six",
+    },
+    {
+      image: "https://i.ibb.co/FkQ1dz4/Whats-App-Image-2025-01-13-at-12-34-59-PM.jpg",
+      name: "Collection Seven",
+    },
+    {
+      image: "https://i.ibb.co/FkQ1dz4/Whats-App-Image-2025-01-13-at-12-34-59-PM.jpg",
+      name: "Collection Eight",
+    },
+  ];
+
+  return (
+    <div id="app">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={20}
+        breakpoints={{
+          768: { slidesPerView: 3 }, // 3 slides on desktop
+        }}
+        pagination={{ clickable: true }}
+        navigation={true}
+        modules={[Navigation, Pagination]}
+        className="mySwiper"
+      >
+        {collections.map((collection, index) => (
+          <SwiperSlide key={index} className="collection-slide">
+            <div className="collection-content">
+              <img
+                src={collection.image}
+                alt={collection.name}
+                className="collection-image"
+              />
+              <div className="collection-overlay">
+                <h3>{collection.name}</h3>
+                <button className="explore-button">Explore Collection</button>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default Slider;
